799fc59cdafbee32c4a4ad14049f07490c91e964
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-18 21:51:07 +0300

    add has_been_read filter to context

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,7 @@ from chat.models import *
 
 
 def show_messages(request):
-    messages = MessageFromSpace.objects.all()
+    messages = MessageFromSpace.objects.filter(has_been_read=False)
     template = 'chat/show_messages.html'
     context = {'messages': messages}
     return render(request, template, context)
