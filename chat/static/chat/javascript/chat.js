a0f52d6c0553326fbcab01d9d6524614d2e0cb2a
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-19 22:37:10 +0300

    add vue method

--- chat/static/chat/javascript/chat.js
+++ chat/static/chat/javascript/chat.js
@@ -2,15 +2,38 @@ $( document ).ready(function() {
 
     Vue.component('message', {
         props: ['msg'],
-        template: '<div>{{ msg.text }}</div>'
+        template: '<div>{{ msg.fields.text }}</div>'
     });
 
     var app1 = new Vue({
       el: '#app1',
-      data: {messages: []}
+      data: {messages: [
+                          { id: 0, pk: 'Vegetables' },
+                          { id: 1, pk: 'Cheese' },
+                          { id: 2, pk: 'Whatever else humans are supposed to eat' }
+                        ],
+      },
+      methods: {
+        update_messages: function() {
+            $.ajax({
+                url: $('#app1').attr('data-ajax-get_new_messages'),
+                type: 'get',
+                dataType: 'json',
+                data: {'last_id': 1},
+                success: function(data) {
+                    app1.messages = data
+                }
+            })
+        }
+      },
     });
 
-    app1.messages = get_values();
+    app1.update_messages();
+
+
+
+
+
 
 });
 
@@ -18,22 +41,26 @@ $( document ).ready(function() {
 
 // ----------------------------------------------- funcs ------------------------------------------------------
 
-function get_values() {
-    return [
-              { id: 0, text: 'Vegetables' },
-              { id: 1, text: 'Cheese' },
-              { id: 2, text: 'Whatever else humans are supposed to eat' }
-           ]
-};
-
-function get_new_messages(handleData) {
-    $.ajax({
-        url: $('#app1').attr('data-ajax-get_new_messages'),
-        type: 'get',
-        dataType: 'json',
-        data: {'last_id': 1},
-        success: function(data) {
-                                    handleData(data);
-                                }
-    })
-};
+//function get_values() {
+//    return [
+//              { id: 0, text: 'Vegetables' },
+//              { id: 1, text: 'Cheese' },
+//              { id: 2, text: 'Whatever else humans are supposed to eat' }
+//           ]
+//}
+//
+//function get_new_messages() {
+//            $.ajax({
+//                url: $('#app1').attr('data-ajax-get_new_messages'),
+//                type: 'get',
+//                dataType: 'json',
+//                data: {'last_id': 1},
+//                success: function(data) {
+//                    app1.messages = [
+//                         { id: 0, text: 'Vegasdaasdasdasdasdetables' },
+//                         { id: 1, text: 'Cheese' },
+//                         { id: 2, text: 'Whatever else humans are supposed to eat' }
+//                    ]
+//                }
+//            })
+//        }
