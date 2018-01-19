2ab026f03d1d641fae70d2789245c40f6d5f57d5
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-19 23:53:46 +0300

    add model's `str` method

--- chat/models.py
+++ chat/models.py
@@ -10,6 +10,9 @@ class MessageFromSpace(models.Model):
     text = models.TextField(verbose_name='Текст')
     has_been_read = models.BooleanField(default=False)
 
+    def __str__(self):
+        return '%s, %s' % (self.text, self.has_been_read)
+
     @classmethod
     def get_new_messages(cls, last_id):
         return MessageFromSpace.objects.filter(pk__gt=last_id, has_been_read=False)
