813929adcdb2d2c1f5275a22cdd3d1fd3eb8b572
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-20 14:54:14 +0300

    checkpoint

--- chat/static/chat/javascript/chat.js
+++ chat/static/chat/javascript/chat.js
@@ -1,58 +1,33 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    Vue.component('message', {
-        props: ['message'],
-        template: '<div>{{ message.text }}</div>'
-    });
+  Vue.component('message', {
+    props: ['message'],
+    template: '<div>{{ message.text }}</div>'
+  });
 
-    var app1 = new Vue({
-      el: '#app1',
-      data: {
-        messages: []
-      },
-      methods: {
-        update_messages: function() {
-            $.ajax({
-                url: $('#app1').attr('data-ajax-get_new_messages'),
-                type: 'get',
-                dataType: 'json',
-                data: {'last_id': 0},
-                success: function(data) {
-                    app1.messages = data
-                }
-            })
-        }
-      },
-    });
+  var app1 = new Vue({
+    el: '#app1',
+    data: {
+      messages: [],
+      last_id: 0,
+    },
+    methods: {
+      update_messages: function() {
+        $.ajax({
+          url: $('#app1').attr('data-ajax-get_new_messages'),
+          type: 'get',
+          dataType: 'json',
+          data: {
+            'last_id': app1.last_id,
+          },
+          success: function(data) {
+            app1.messages = data;
+          }
+        })
+      }
+    },
+  });
 
-    app1.update_messages();
+  app1.update_messages();
 
 });
-
-
-
-// ----------------------------------------------- funcs ------------------------------------------------------
-
-//function get_values() {
-//    return [
-//              { id: 0, text: 'Vegetables' },
-//              { id: 1, text: 'Cheese' },
-//              { id: 2, text: 'Whatever else humans are supposed to eat' }
-//           ]
-//}
-//
-//function get_new_messages() {
-//            $.ajax({
-//                url: $('#app1').attr('data-ajax-get_new_messages'),
-//                type: 'get',
-//                dataType: 'json',
-//                data: {'last_id': 1},
-//                success: function(data) {
-//                    app1.messages = [
-//                         { id: 0, text: 'Vegasdaasdasdasdasdetables' },
-//                         { id: 1, text: 'Cheese' },
-//                         { id: 2, text: 'Whatever else humans are supposed to eat' }
-//                    ]
-//                }
-//            })
-//        }
