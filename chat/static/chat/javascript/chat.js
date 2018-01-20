608fe64b7a1e8f9483b237683fd94a32d71a678d
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-21 01:08:46 +0300

    messages update

--- chat/static/chat/javascript/chat.js
+++ chat/static/chat/javascript/chat.js
@@ -13,21 +13,59 @@ $(document).ready(function() {
     },
     methods: {
       update_messages: function() {
+        var self = this;
         $.ajax({
-          url: $('#app1').attr('data-ajax-get_new_messages'),
+          url: '/api/get_messages',
           type: 'get',
           dataType: 'json',
           data: {
-            'last_id': app1.last_id,
+            'last_id': self.last_id,
           },
-          success: function(data) {
-            app1.messages = data;
+          success: function(new_messages) {
+            self.messages.push.apply(self.messages, new_messages);
           }
         })
-      }
+      },
+    },
+    mounted: function() {
+      this.update_messages();
+      setInterval(this.update_messages, 5000);
     },
   });
 
-  app1.update_messages();
-
 });
+
+
+//$(document).ready(function() {
+//
+//  Vue.component('message', {
+//    props: ['message'],
+//    template: '<div>{{ message.text }}</div>'
+//  });
+//
+//  var app1 = new Vue({
+//    el: '#app1',
+//    data: {
+//      messages: [],
+//      last_id: 0,
+//    },
+//    methods: {
+//      update_messages: function() {
+//        $.ajax({
+//          url: $('#app1').attr('data-ajax-get_new_messages'),
+//          type: 'get',
+//          dataType: 'json',
+//          data: {
+//            'last_id': app1.last_id,
+//          },
+//          success: function(data) {
+//            app1.messages = data;
+//          }
+//        })
+//      }
+//    },
+//  });
+//
+//  app1.update_messages();
+//
+//});
