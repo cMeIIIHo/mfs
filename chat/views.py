7fd59beff3b5d049a4bd076a2af8ae91a4d0ba1a
cMeIIIHo <cMeIIIHo7@gmail.com>
2018-01-22 04:42:43 +0300

    add exception words

--- chat/views.py
+++ chat/views.py
@@ -14,23 +14,23 @@ def get_new_messages(request):
     try:
         last_message_id = int(request.GET['last_id'])
     except (KeyError, ValueError):
-        pass
+        raise Http404('invalid parameter or empty')
     else:
         new_messages = MessageFromSpace.get_new_messages(last_message_id)
-        return JsonResponse(list(new_messages.values('id', 'text', 'date')), safe=False)
-    raise Http404
+        new_messages_data = list(new_messages.values('id', 'text', 'date'))
+        return JsonResponse(new_messages_data, safe=False)
 
 
 def mark_message_read(request):
     try:
         message_id = int(request.GET['message_id'])
     except (KeyError, ValueError):
-        pass
+        raise Http404('invalid parameter or empty')
     else:
         try:
             message = MessageFromSpace.objects.get(pk=message_id)
         except ObjectDoesNotExist:
-            pass
+            raise Http404('No messages with id > %s' % message_id)
         else:
             message.has_been_read = True
             message.save()
